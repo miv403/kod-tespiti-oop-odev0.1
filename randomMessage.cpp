f9ac0bf78d16c17ad030642126de382caabefeda
miv403 <miv403@duck.com>
2024-10-28 20:42:02 +0300

    randomMessage oluşturucu eklendi.

---
+++ randomMessage.cpp
@@ -0,0 +1,23 @@
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <fstream>
+
+#define MAX 250
+
+int main() {
+
+    std::srand(std::time(nullptr));
+
+    std::ofstream file;
+    file.open("./random.msg");
+    file << MAX;
+
+    for(int i = 0; i < MAX; ++i){
+        file << " " << std::rand() % 10;
+    }
+
+    file.close();
+    return 0;
+
+}
